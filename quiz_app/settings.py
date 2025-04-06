97ed8cfebd28dbb19a8823ab23bb2fba8d820c00
Amirkhan991197 <khanamir991197@gmail.com>
2025-04-06 21:47:53 +0530

    Updated ALLOWED_HOSTS for Render

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -144,6 +144,7 @@ LOGOUT_REDIRECT_URL = "login"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / 'quiz' / 'static' ]

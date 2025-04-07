0a18b2252dbde843412a78b7bea505e8c7323f3c
Amirkhan991197 <khanamir991197@gmail.com>
2025-04-07 19:20:34 +0530

    Update settings.py for Render deployment

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -72,8 +72,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "hello123@.com"  # Replace with your Gmail
-EMAIL_HOST_PASSWORD = "iscb isdv vjka awcd"  # App password (not your Gmail password)
+EMAIL_HOST_USER = "khanamir991197@.com"  # Replace with your Gmail
+EMAIL_HOST_PASSWORD = "dkpw jqrw cbqz duor"  # App password (not your Gmail password)
 
 # Internationalization
 LANGUAGE_CODE = 'en-us'

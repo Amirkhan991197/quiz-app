e045f562d4787a1019ee0c7abe951da60cad168b
Amirkhan991197 <khanamir991197@gmail.com>
2025-04-07 19:51:25 +0530

    Update settings.py for Render deployment

--- quiz_app/model.py
+++ quiz_app/model.py
@@ -26,3 +26,11 @@ class UserResponse(models.Model):
 
     def __str__(self):
         return f"{self.user_name} - {self.question}"
+
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title   #ye krne se error fix ho jayega  (if not remove it)
